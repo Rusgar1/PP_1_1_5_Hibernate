00e1ccc62a5c3811981dd60ef84346d9a4a67ca5
Rusgar1 <grr7727@gmail.com>
2023-04-18 11:11:11 +0400

    fix: class Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -16,15 +16,18 @@ import java.util.Properties;
 public class Util {
     // реализуйте настройку соеденения с БД
 
-    //JDBC
+
     private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DB_URL = "jdbc:mysql://localhost:3306/db1_4";
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "root";
 
+    private static SessionFactory sessionFactory;
+
+    //JDBC
     public static Connection getConn() {
         Connection connection = null;
-        try{
+        try {
             Class.forName(DB_DRIVER);
             connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
         } catch (ClassNotFoundException | SQLException e) {
@@ -34,17 +37,16 @@ public class Util {
     }
 
     //Hibernate
-    private static SessionFactory sessionFactory;
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
             try {
                 Configuration configuration = new Configuration();
 
                 Properties settings = new Properties();
-                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/db1_4");
-                settings.put(Environment.USER, "root");
-                settings.put(Environment.PASS, "root");
+                settings.put(Environment.DRIVER, DB_DRIVER);
+                settings.put(Environment.URL, DB_URL);
+                settings.put(Environment.USER, DB_USERNAME);
+                settings.put(Environment.PASS, DB_PASSWORD);
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
                 settings.put(Environment.SHOW_SQL, "true");
                 settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
